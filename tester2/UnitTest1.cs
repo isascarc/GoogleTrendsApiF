4a90353d90fe52e318522e08a0871b4a0ee9a1d3
isascarc <isascarch@gmail.com>
2023-04-23 13:12:19 +0300

    Add tests

---
+++ tester2/UnitTest1.cs
@@ -0,0 +1,25 @@
+using GoogleTrendsApi;
+
+namespace tester;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup() { }
+
+    [Test]
+    public void Test1()
+    {
+        var t = Api.GetTopCharts(2022).Result;
+        Assert.That(t, Has.Count.GreaterThan(1));
+
+        var t2 = Api.GetAllTrendingSearches().Result;
+        Assert.That(t2, Is.Not.Null);
+
+        var t3 = Api.GetTrendingSearches("israel").Result;
+        Assert.That(t3, Is.Not.Null);
+
+        var t4 = Api.GetCategories().Result;
+        Assert.That(t4, Is.Not.Null);
+    }
+}
