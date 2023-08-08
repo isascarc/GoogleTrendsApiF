73fcf56fafb6a6390360b8146a0fe1c5c3b86382
isascarc <isascarch@gmail.com>
2023-08-08 10:10:13 +0300

    Readability

---
+++ src/Usings.cs
@@ -0,0 +1,6 @@
+global using System;
+global using System.Text.Json;
+global using System.Threading.Tasks;
+global using System.Collections.Generic;
+global using System.Net;
+global using System.Net.Http;
